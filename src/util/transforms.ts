c67950f47d73992d1110256675a5b5fc90e42825
Paul Marshall <pmarshall@quantummetric.com>
2024-03-12 11:46:49 -0700

    feat: add in ootb event mappings and transformers
    feat: add in event transformers
    
    simplify types
    
    typo
    
    rename mde to attributes

---
+++ src/util/transforms.ts
@@ -0,0 +1,93 @@
+import { AnalyticsEvent } from "@backstage/core-plugin-api";
+import { AnalyticsEvent as NewAnalyticsEvent } from "@backstage/frontend-plugin-api";
+
+/*
+ * Transformers take in Backstage analytics events and transform the data to fit what is expected by the Quantum Metric
+ * API.
+ */ 
+export type Transformer = (
+  event: AnalyticsEvent | NewAnalyticsEvent,
+  eventMapping: Record<
+    AnalyticsEvent["action"] | NewAnalyticsEvent["action"],
+    QuantumEvent["eventId"]
+  >
+) => QuantumEvent;
+
+type QuantumEvent = {
+  eventId: number | string;
+  conversion?: boolean | number;
+  eventValue?: number | string;
+  attributes?: Record<string, string | boolean | number | undefined>;
+};
+
+// Generic "sendEvent" actions
+const sendEventTransform: Transformer = (event) => {
+  const { subject, value, attributes } = event;
+  const quantumEvent = {
+    eventId: subject,
+    eventValue: value,
+    attributes: attributes,
+    conversion: false,
+  };
+  return quantumEvent;
+};
+
+// OOTB "Navigation" actions
+const navigationEventTransform: Transformer = (event, eventMapping) => {
+  const { action, subject, attributes } = event;
+  const quantumEvent = {
+    eventId: eventMapping[action],
+    eventValue: subject,
+    attributes: attributes,
+    conversion: false,
+  };
+
+  return quantumEvent;
+};
+
+// OOTB "Search" actions
+const searchEventTransform: Transformer = (event, eventMapping) => {
+  const { action, subject, value, attributes } = event;
+  const quantumEvent = {
+    eventId: eventMapping[action],
+    eventValue: subject,
+    attributes: { ...attributes, "results-found": value },
+    conversion: false,
+  };
+
+  return quantumEvent;
+};
+
+// OOTB "Discover" actions
+const discoverEventTransform: Transformer = (event, eventMapping) => {
+  const { action, subject, value, attributes } = event;
+  const quantumEvent = {
+    eventId: eventMapping[action],
+    eventValue: subject,
+    attributes: { ...attributes, "search-position": value },
+    conversion: false,
+  };
+
+  return quantumEvent;
+};
+
+export const defaultEventTransform: Transformer = (event, eventMapping) => {
+  const { action, subject, value, attributes } = event;
+  const quantumEvent = {
+    eventId: eventMapping[action],
+    eventValue: value ?? subject,
+    attributes: attributes,
+    conversion: false,
+  };
+
+  return quantumEvent;
+};
+
+export const defaultTransforms: Record<string, Transformer> = {
+  navigate: navigationEventTransform,
+  search: searchEventTransform,
+  discover: discoverEventTransform,
+  "not-found": defaultEventTransform,
+  click: defaultEventTransform,
+  sendEvent: sendEventTransform,
+};
