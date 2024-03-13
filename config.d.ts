039229c300d9fbef6d589e4a21fcf94fd9893e43
Paul Marshall <pmarshall@quantummetric.com>
2024-03-13 08:37:20 -0700

    feat: add in global attributes in config

--- config.d.ts
+++ config.d.ts
@@ -4,48 +4,56 @@ export interface Config {
       qm: {
         /**
          * Controls if plugin is on or off
-         * NOTE: Visibility applies to only this field
          * @visibility frontend
          */
         enabled: boolean;
         /**
          *  Turns on console.debug messages
-         * NOTE: Visibility applies to only this field
          * @visibility frontend
          */
         debug: boolean;
         /**
          * Sends events to console log instead of quantum metric
-         * NOTE: Visibility applies to only this field
          * @visibility frontend
          */
         test: boolean;
         /**
          * Quantum Metric CDN source URL
-         * NOTE: Visibility applies to only this field
          * @visibility frontend
          */
         src: string;
         /**
          * Controls wether or not to block on Quantum Metric API
-         * NOTE: Visibility applies to only this field
          * @visibility frontend
          */
         async: number;
-        /**
-         * Global attributes to be included on all events
-         * NOTE: Visibility applies to only this field
-         * @visibility frontend
-         */
-        attributes: any;
 
-        /**
-         * Event mapping of Backstage events like navigation/search to their Quantum Metric event IDs
-         * @deepvisibility frontend
-         */
+        // Global attributes to be included on all events
+        attributes: {
+          /**
+           * Key of global attribute field to include on all events sent to Quantum Metric
+           * @visibility frontend
+           */
+          name: string;
+          /**
+           * Value of global attribute field to include on all events sent to Quantum Metric
+           * @visibility frontend
+           */
+          value: string | number | boolean;
+        }[];
+
+        // Event mapping of Backstage events like navigation/search to their Quantum Metric event IDs
         events: {
           mappings: {
+            /**
+             * Backstage event action like 'search' || 'click' to map to the Quantum Metric event ID
+             * @visibility frontend
+             */
             name: string;
+            /**
+             * Quantum Metric event ID to send when the above event name is detected.
+             * @visibility frontend
+             */
             id: number | string;
           }[];
         };
